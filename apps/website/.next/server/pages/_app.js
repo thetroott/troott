0b512abf60d1aadb9e29fdda7089d79d00d1f24f
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-04-07 14:35:38 +0100

    chore: stop tracking website Next.js build output

--- apps/website/.next/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0994584b._.js")
-R.m("[project]/node_modules/.pnpm/next@15.5.12_@babel+core@7.29.0_@opentelemetry+api@1.9.0_babel-plugin-react-compiler@1._ac40b0ab46bf2d6582b311d9e120f7e8/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/.pnpm/next@15.5.12_@babel+core@7.29.0_@opentelemetry+api@1.9.0_babel-plugin-react-compiler@1._ac40b0ab46bf2d6582b311d9e120f7e8/node_modules/next/app.js [ssr] (ecmascript)").exports
--- apps/website/.next/static/chunks/029d7_next_app_0b100b76.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/node_modules/.pnpm/next@15.5.12_@babel+core@7.29.0_@opentelemetry+api@1.9.0_babel-plugin-react-compiler@1._ac40b0ab46bf2d6582b311d9e120f7e8/node_modules/next/app.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/node_modules/.pnpm/next@15.5.12_@babel+core@7.29.0_@opentelemetry+api@1.9.0_babel-plugin-react-compiler@1._ac40b0ab46bf2d6582b311d9e120f7e8/node_modules/next/dist/pages/_app.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=029d7_next_app_0b100b76.js.map
