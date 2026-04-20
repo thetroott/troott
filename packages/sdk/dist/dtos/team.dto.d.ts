97c8fe404faef61e0699e8f399864f0a1e84f70f
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-04-20 18:45:24 +0100

    chore(sdk): SDK package source and dist (3/13)
    Batched commit 3 of 13 (~18 paths per batch).
    Scope: SDK package source and dist.

--- packages/sdk/dist/dtos/team.dto.d.ts
+++
@@ -1,5 +0,0 @@
-interface Team {
-    [key: string]: unknown;
-}
-export default Team;
-//# sourceMappingURL=team.dto.d.ts.map
--- packages/sdk/dist/dtos/template.dto.d.ts
+++
@@ -1,9 +0,0 @@
-interface Template {
-    createdAt: Date;
-    updatedAt: Date;
-    _version: number;
-    _id: any;
-    id: any;
-}
-export default Template;
-//# sourceMappingURL=template.dto.d.ts.map
