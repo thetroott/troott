4605bfc972ea8249b134acf8e75fa7ddd8921585
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-04-20 18:45:24 +0100

    chore(api): API modular migration and config (15/25)
    Batched commit 15 of 25 (~18 paths per batch).
    Scope: API modular migration and config.

---
+++ apps/api/src/modules/users/listener/listener.dto.ts
@@ -0,0 +1,49 @@
+import { IUserDoc } from '../user/user.interface';
+import { IDebitCard } from '../../shared/interfaces/card.interface';
+
+export interface CreateListenerDTO {
+    user: IUserDoc;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    gender?: string;
+    avatar?: string;
+    dateOfBirth?: Date;
+    country?: string;
+    phoneNumber?: string;
+    phoneCode?: string;
+    slug?: string;
+    createdBy?: string;
+}
+
+export interface UpdateListenerDTO {
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    gender?: string;
+    avatar?: string;
+    dateOfBirth?: Date;
+    country?: string;
+    phoneNumber?: string;
+    phoneCode?: string;
+    countryPhone?: string;
+    slug?: string;
+    card?: IDebitCard;
+    interests?: string[];
+    badges?: string[];
+}
+
+export interface InviteListenerDTO {
+    email: string;
+    resourceId?: string;
+}
+
+export interface AcceptListenerInvitationDTO {
+    token: string;
+    email: string;
+    password: string;
+}
+
+export interface SetListenerPasswordDTO {
+    password: string;
+}
