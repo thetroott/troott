cef7e809dd4f95c2251f05375668ea5212f7c429
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-04-28 20:42:10 +0100

    chore(repo): remove sdk state and storage
    Delete deprecated SDK state/storage layers and include the API
    audio processing job plan used for migration context.

--- packages/sdk/src/storage/index.ts
+++
@@ -1,4 +0,0 @@
-export type { IStorageAdapter } from './storage.interface';
-export { AUTH_STORAGE_KEYS, STORAGE_KEYS } from './constants';
-export { LocalStorageAdapter } from './local-storage-adapter';
-export { AsyncStorageAdapter, type IAsyncKeyValueStore } from './async-storage-adapter';
