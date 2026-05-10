d5f511b7188207c4535af38be050bd74c655a677
Damola Oladipo <thedamolaoladipo@gmail.com>
2026-05-10 21:22:09 +0100

    chore: remove deprecated native-ui package

--- packages/native-ui/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './components/Button';
-export * from './components/Text';
-export * from './components/Card';
-export * from './components/Input';
-export * from './components/Layout';
